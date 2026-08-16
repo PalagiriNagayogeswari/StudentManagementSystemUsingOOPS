55ec8fbb55d1f6be071b81c74f34fe7d2a291284
PalagiriNagayogeswari <yogi34343@gmail.com>
2026-08-16 08:05:10 +0530

    [Nagayogeswari]:added Database connection file.

---
+++ src/main/java/com/bridgelabz/util/DBConnection.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/studentdb";
+
+    private static final String USER = "postgres";
+
+    private static final String PASSWORD = "Yogi@180523";
+
+    public static Connection getConnection() {
+
+        Connection con = null;
+
+        try {
+
+            Class.forName("org.postgresql.Driver");
+
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
+
+        }
+
+        catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+        return con;
+
+    }
+
+}
