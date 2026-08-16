ab6916e50f3d2aaf7d536170a549946645934134
PalagiriNagayogeswari <yogi34343@gmail.com>
2026-08-16 07:59:31 +0530

    [Nagayogeswari]:added Student DAO interface file.

---
+++ src/main/java/com/bridgelabz/dao/StudentDAO.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.dao;
+
+import com.bridgelabz.model.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+    void addStudent(Student student);
+
+    List<Student> getAllStudents();
+
+    Student getStudentById(int id);
+
+    void updateStudent(Student student);
+
+    void deleteStudent(int id);
+
+}
